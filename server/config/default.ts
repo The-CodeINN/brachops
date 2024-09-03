03bf1a98a44e152e5285f26f36cddad3fb89796f
Christian <christianichebi@gmail.com>
2024-09-04 00:12:09 +0100

    Added validators and updated controllers

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,6 +1,6 @@
 export default {
   port: 3000,
-  jenkinsUserName: "Jaykold",
-  jenkinsPassword: "Cucurell@123",
-  jenkinsUrl: "192.168.210.101:8080",
+  jenkinsUserName: "username",
+  jenkinsPassword: "password",
+  jenkinsUrl: "0.0.0.0:8080",
 };
